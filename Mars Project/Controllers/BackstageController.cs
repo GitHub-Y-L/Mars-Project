c587128f0ea7caa64ceb973fba855aa459545d03
MylesZR <crawling48@outlook.com>
2019-12-13 16:52:52 +0800

    delel

--- Mars Project/Controllers/BackstageController.cs
+++ Mars Project/Controllers/BackstageController.cs
@@ -12,5 +12,10 @@ namespace Mars_Project.Controllers
         {
             return View();
         }
+
+        public ActionResult dele()
+        {
+            return View();
+        }
     }
 }
