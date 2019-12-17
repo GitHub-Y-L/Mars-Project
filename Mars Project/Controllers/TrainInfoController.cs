7c2c446361c8a522eb29391ceb1e61657f5abc88
alphaAE <a1226123914@gmail.com>
2019-12-17 15:25:28 +0800

    添加数据库模型，创建旅游信息控制器

---
+++ Mars Project/Controllers/TrainInfoController.cs
@@ -0,0 +1,36 @@
+﻿using Mars_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mars_Project.Controllers
+{
+    public class TrainInfoController : Controller
+    {
+        // GET: TrainInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPageInfo(int page, int limit) {
+            var db = new MarsProjectDBEntities();
+            int countData = db.TrainInfo.Count();
+            List<TrainInfo> dataList = db.TrainInfo
+                .OrderBy(s => s.Id)
+                .Skip((page - 1) * limit)
+                .Take(limit)
+                .ToList();
+
+            var msg = new {
+                code = 0,
+                msg = "",
+                count = countData,
+                data = dataList
+            };
+            return Json(msg);
+        }
+    }
+}
