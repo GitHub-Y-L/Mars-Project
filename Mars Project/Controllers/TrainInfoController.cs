3d632355ac98d38b052a42fc8e0c6f3a3529271f
alphaAE <a1226123914@gmail.com>
2019-12-17 16:18:36 +0800

    将UEditor添加到项目，修改Json版本

--- Mars Project/Controllers/TrainInfoController.cs
+++ Mars Project/Controllers/TrainInfoController.cs
@@ -32,5 +32,29 @@ namespace Mars_Project.Controllers
             };
             return Json(msg);
         }
+
+        public ActionResult AddInfo() {
+            return PartialView();
+        }
+
+        public ActionResult DoAddInfo(TrainInfo trainInfo) {
+            var db = new MarsProjectDBEntities();
+            db.TrainInfo.Add(trainInfo);
+            var codeData = db.SaveChanges();
+            var msg = new {
+                code = codeData,
+            };
+            return Json(msg);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult UeTest(FormCollection fc) {
+            var content = fc["editor"];
+
+            return View();
+        }
+
+
     }
 }
