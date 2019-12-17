f64e4fb8994b74ea6b56c104dcab5d85ce31b70b
alphaAE <a1226123914@gmail.com>
2019-12-17 20:47:41 +0800

    培训列表控制器完成，富文本编辑器存在问题

--- Mars Project/Controllers/TrainInfoController.cs
+++ Mars Project/Controllers/TrainInfoController.cs
@@ -19,6 +19,7 @@ namespace Mars_Project.Controllers
             var db = new MPEntities();
             int countData = db.TrainInfoes.Count();
             List<TrainInfo> dataList = db.TrainInfoes
+                .Where(s=>s.IsDele == 0)
                 .OrderBy(s => s.Id)
                 .Skip((page - 1) * limit)
                 .Take(limit)
@@ -33,6 +34,7 @@ namespace Mars_Project.Controllers
             return Json(msg);
         }
 
+
         public ActionResult AddInfo() {
             return PartialView();
         }
@@ -41,12 +43,47 @@ namespace Mars_Project.Controllers
         [ValidateInput(false)]
         public ActionResult DoAddInfo(TrainInfo trainInfo) {
             var db = new MPEntities();
-            //db.TrainInfo.Add(trainInfo);
-            //var codeData = db.SaveChanges();
-            //var msg = new {
-            //    code = codeData,
-            //};
-            return Json(0);
+            trainInfo.CreateTime = DateTime.Now;
+            db.TrainInfoes.Add(trainInfo);
+            var codeData = db.SaveChanges();
+            var msg = new {
+                code = codeData,
+            };
+            return Json(msg);
+        }
+
+
+        [HttpPost]
+        public ActionResult DoDeleInfo(int id) {
+            var db = new MPEntities();
+            TrainInfo trainInfo = db.TrainInfoes.Where(s => s.Id == id).FirstOrDefault();
+            //移除对象
+            // db.NavMenus.Remove(temp);
+            trainInfo.IsDele = 1;
+            
+            int i = db.SaveChanges();
+            var res = new { code = i };
+            return Json(res);
+        }
+
+
+        public ActionResult ModifyInfo(int id) {
+            var db = new MPEntities();
+            var trainInfo = db.TrainInfoes.Where(s => s.Id == id).FirstOrDefault();
+            return PartialView(trainInfo);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult DoModifyInfo(TrainInfo trainInfo) {
+            var db = new MPEntities();
+            TrainInfo temp = db.TrainInfoes.Attach(trainInfo);
+            temp.ModifyTime = DateTime.Now;
+            //修改对象在内存中的状态
+            db.Entry<TrainInfo>(temp).State = System.Data.Entity.EntityState.Modified;
+            int i = db.SaveChanges();
+            var res = new { code = i };
+            return Json(res);
         }
 
     }
