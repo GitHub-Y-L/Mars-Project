a9f8a3ff7a955cc6016173323d2acf5ccb097b18
alphaAE <a1226123914@gmail.com>
2019-12-24 09:05:21 +0800

    导航栏控制器重做

--- Mars Project/Controllers/NavMenuController.cs
+++ Mars Project/Controllers/NavMenuController.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 using Mars_Project.Models;
 
-namespace Mars_Project.Controllers
-{
+namespace Mars_Project.Controllers {
 
-    public class NavMenuController : Controller
-    {
+    public class NavMenuController : Controller {
         //
         // GET: /NavMenu/
 
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
@@ -24,13 +22,12 @@ namespace Mars_Project.Controllers
         /// 用于获取数据分页
         /// </summary>
         /// <returns></returns>
-        public ActionResult GetPageData(int page, int limit)
-        {
+        public ActionResult GetPageData(int page, int limit) {
             var db = new MPEntities();
             // 统计表中的记录数：select count(*) from [dbo].[NavMenus] where isdele=0
             int count = db.NavMenus.Where(s => s.IsDele == 0).Count();
 
-            
+
 
             //lambda 表达式
             List<NavMenu> dataList = db.NavMenus
@@ -54,8 +51,7 @@ namespace Mars_Project.Controllers
         /// 添加信息的界面
         /// </summary>
         /// <returns></returns>
-        public ActionResult AddNavMenu()
-        {
+        public ActionResult AddNavMenu() {
             return PartialView();
         }
 
@@ -64,11 +60,26 @@ namespace Mars_Project.Controllers
         /// </summary>
         /// <param name="stu"></param>
         /// <returns></returns>
-        public ActionResult DoAddNavMenu(NavMenu menu)
-        {
+        public ActionResult DoAddNavMenu(NavMenu menu, HttpPostedFileBase file) {
             var db = new MPEntities();
-            //AdminInfo info = Session["admin"] as AdminInfo;
-            //menu.CreaterId = info.Id;// 从 登录信息中 获取 登录人的 Id【从登录Session 中获取】
+
+            if (file != null) {
+                //用于同名图片处理，设置物理路径
+                string fileName = "~/UploadFiles/" + DateTime.Now.ToString("yyyyMMddHHssmm") + Path.GetFileName(file.FileName);
+                var physicsFileName = Server.MapPath(fileName);
+                try {
+                    //保存图片到对应目录
+                    file.SaveAs(physicsFileName);
+                    menu.TopBanner = fileName.Replace("~", "");
+                } catch (Exception e) {
+                    return Json(new { result = "false", code = 500, msg = "保存失败" }, "text/html");
+                }
+            } else {
+                return Json(new { result = "false", code = 500, msg = "文件不存在" }, "text/html");
+            }
+
+            AdminInfo info = Session["admin"] as AdminInfo;
+            menu.CreaterId = info.Id;
             menu.CreateTime = DateTime.Now;
             db.NavMenus.Add(menu);
             int i = db.SaveChanges();
@@ -83,17 +94,13 @@ namespace Mars_Project.Controllers
         /// 修改信息的界面
         /// </summary>
         /// <returns></returns>
-        public ActionResult ModeifyNavMenu(int id)
-        {
+        public ActionResult ModeifyNavMenu(int id) {
             var db = new MPEntities();
             var obj = db.NavMenus.Where(s => s.Id == id).FirstOrDefault();
 
-            if (obj == null)
-            {
+            if (obj == null) {
                 return Content("修改数据不存在！");
-            }
-            else
-            {
+            } else {
                 return PartialView(obj);
             }
 
@@ -105,10 +112,22 @@ namespace Mars_Project.Controllers
         /// </summary>
         /// <param name="stu"></param>
         /// <returns></returns>
-        public ActionResult DoModifyNavMenu(NavMenu menu)
-        {
+        public ActionResult DoModifyNavMenu(NavMenu menu, HttpPostedFileBase file) {
             //创建数据对象
             var db = new MPEntities();
+
+            if (file != null) {
+                //用于同名图片处理，设置物理路径
+                string fileName = "~/UploadFiles/" + DateTime.Now.ToString("yyyyMMddHHssmm") + Path.GetFileName(file.FileName);
+                var physicsFileName = Server.MapPath(fileName);
+                try {
+                    //保存图片到对应目录
+                    file.SaveAs(physicsFileName);
+                    menu.TopBanner = fileName.Replace("~", "");
+                } catch (Exception e) {
+                    return Json(new { result = "false", code = 500, msg = "保存失败" }, "text/html");
+                }
+            }
             //附加对象
             NavMenu temp = db.NavMenus.Attach(menu);
             AdminInfo info = Session["admin"] as AdminInfo;
@@ -131,8 +150,7 @@ namespace Mars_Project.Controllers
         /// </summary>
         /// <param name="id">主键Id</param>
         /// <returns></returns>
-        public ActionResult DoDele(int id)
-        {
+        public ActionResult DoDele(int id) {
             //创建数据对象
             var db = new MPEntities();
             //根据传入id 查找 对象
